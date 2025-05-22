fd650ebc5328d26f661ef3b5485208f57fe6ddd6
andreljungsvik <andre.ljungsvik@gmail.com>
2025-05-22 10:31:44 +0200

    use joinpoint on advice class

--- src/main/java/se/yrgo/advice/PerformanceTimingAdvice.java
+++ src/main/java/se/yrgo/advice/PerformanceTimingAdvice.java
@@ -2,26 +2,28 @@ package se.yrgo.advice;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
-public class PerformanceTimingAdvice implements MethodInterceptor {
-
-    @Override
-    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+@Aspect
+@Component
+public class PerformanceTimingAdvice {
+    @Around("execution(* se.yrgo.services..*.*(..)) || execution(* se.yrgo.dataaccess..*.*(..))")
+    public Object measureExecutionTime(ProceedingJoinPoint jp) throws Throwable {
         long startTime = System.nanoTime();
 
         try {
-            return methodInvocation.proceed();
+            return jp.proceed();
         } finally {
             long endTime = System.nanoTime();
-            double timeTakenMs = (endTime - startTime) / 1000000.0;
+            double duration = (endTime - startTime) / 1_000_000.0;
 
-            String methodName = methodInvocation.getMethod().getName();
-            String className = methodInvocation.getMethod().getDeclaringClass().getName();
-
-            System.out.printf(
-                    "Time taken for the method %s from the class %s took %.4fms%n",
-                    methodName, className, timeTakenMs
-            );
+            System.out.printf("Time taken for the method %s from the class %s took %.4fms%n",
+                    jp.getSignature().getName(),
+                    jp.getSignature().getDeclaringTypeName(),
+                    duration);
         }
     }
 }
